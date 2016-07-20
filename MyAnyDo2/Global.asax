61c1de42bdd5ecd39809347062d5e204569f6874
Dina Bojaruneca <dina.bojaruneca@outlook.com>
2016-07-20 13:34:11 +0300

    Add project files.

---
+++ MyAnyDo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyAnyDo2.MvcApplication" Language="C#" %>
---
+++ MyAnyDo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyAnyDo2.Startup))]
+namespace MyAnyDo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
